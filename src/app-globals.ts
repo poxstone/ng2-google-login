3446c20dbdee7cc957d8b60bc9b38b1769a25120
oscar ortiz <oscar.ortiz@eforcers.com>
2017-06-11 23:16:51 -0500

    init

---
+++ src/app-globals.ts
@@ -0,0 +1,7 @@
+/**
+ * Global constants. You can define your constant and access it by importing this file in the component.
+ */
+export const AppGlobals = {
+    GOOGLE_CLIENT_ID: '',
+    SCOPES: ''
+}
