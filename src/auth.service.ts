22a4e1bacb432212ff1bcb08ea2c14e3432f5f4a
oscar ortiz <oscar.ortiz@eforcers.com>
2017-06-21 00:27:38 -0500

    add login and auto login endpoints

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@angular/core';
 import { AppGlobals } from './app-globals';
 
+// get window
+function getWindow (): any {
+  return window;
+}
 declare const gapi: any;
 
 @Injectable()
 export class AuthService {
-  constructor() { }
+  private APIROOT = 'endpoint2-dot-efor-gae-temp-01.appspot.com';
+  private APILOCAL = 'localhost:8080';
+  private EXTAPI = false; // true force to load APIROOT from local
+  private gapi:any = {};
+  private apiName:string;
+  private apiVersion:string;
+
+  constructor() {
+    this.gapi = getWindow().gapi;
+  }
 
   /**
    * Calling Google login API and fetching account details.
    * @param callback Callback to function
    */
-  public authenticateUser(callback) {
+  public apiInit(apiName:string, apiVersion:string, callback) {
     let auth2: any;
     let result: any;
     let error: any;
+    let _thisClass = this;
 
     gapi.load('auth2', function () {
       auth2 = gapi.auth2.init({
@@ -26,23 +40,11 @@ export class AuthService {
       //Login button reference
       let loginButton: any = document.getElementById('google-login-button');
 
-      auth2.attachClickHandler(
-        loginButton, 
-        {},
-        function success(userDetails) {
+      auth2.attachClickHandler( loginButton, {}, function success(userDetails) {
           //Getting profile object
           let profile = userDetails.getBasicProfile();
 
-          // Alternatively you can create an object and return it like that -
-           result = {
-             token: userDetails.getAuthResponse().id_token,
-             name: profile.getName(),
-             image: profile.getImageUrl(),
-             email: profile.getEmail(),
-           };
-
-          //Setting data to localstorage.
-          localStorage.setItem('login', JSON.stringify(result));
+          result = _thisClass.saveLogin(userDetails.getAuthResponse().id_token, profile);
 
           callback(result);
 
@@ -52,15 +54,86 @@ export class AuthService {
         });
 
     });
+
+    this.apiAutoInit(apiName, apiVersion, callback);
   }
 
-  /**
-   * Logout user from Google
-   * @param callback Callback to function
-   */
-  userLogout(callback) {
-    //You will be redirected to this URL after logging out from Google.
-    let homeUrl = "http://localhost:4200";
+  private saveLogin(token:string, profile:any):any {
+    let result:any;
+
+    if (profile.getName) {
+      result = {
+        token: token,
+        name: profile.getName(),
+        image: profile.getImageUrl(),
+        email: profile.getEmail()
+      };
+    } else {
+      result = {
+        token: token,
+        name: profile['name'],
+        image: profile['picture'],
+        email: profile['email']
+      };
+    }
+
+    //Setting data to localstorage.
+    localStorage.setItem('login', JSON.stringify(result));
+    return result;
+  }
+
+  public apiAutoInit (apiName:string, apiVersion:string, callback) {
+    this.apiName = apiName;
+    this.apiVersion = apiVersion;
+    let host = location.host.match(/localhost/) ? this.APILOCAL : this.APIROOT ;
+    host = this.EXTAPI ? this.APIROOT : host;
+    let apiRoot = '//' + host + '/_ah/api';
+    var apisToLoad;
+
+    var callbackInint = () => {
+      // 2 calls helloworld and oauth2
+      if (--apisToLoad == 0) {
+        // enableButtons();
+        console.log( 'autenticando...' );
+        this.signin( true, this.userAuthed(callback) );
+      }
+    }
+
+    apisToLoad = 2; // must match number of calls to gapi.client.load()
+
+    // gapi was loaded
+    if ( this.gapi.client && this.gapi.client.load ) {
+      this.gapi.client.load(this.apiName, this.apiVersion, callbackInint, apiRoot); // load Api
+      this.gapi.client.load('oauth2', 'v2', callbackInint);
+    } else {
+      setTimeout(() => { this.apiInit(apiName, apiVersion, callback) }, 250)
+    }
+
+  }
+
+  public signin(mode, callbackUserAuthed) {
+    let authObj = { 'client_id': AppGlobals.GOOGLE_CLIENT_ID,
+      'scope':      AppGlobals.SCOPES,
+      'immediate': mode };
+    this.gapi.auth.authorize(authObj, callbackUserAuthed); //apiHello.userAuthed
+  };
+
+  private userAuthed(callback) { // is executed in another context
+    return () => {
+      let gapi =  getWindow().gapi;
+      let request = gapi.client.oauth2.userinfo.get().execute( respiUserAuth => {
+      let result;
+        if (!respiUserAuth.code) {
+          console.log('loging true', respiUserAuth);
+          result = this.saveLogin('', respiUserAuth);
+          callback(result);
+        }
+      });
+    }
+  };
+
+  public userLogout(callback) {
+    let homeUrl = location.origin; // "http://localhost:4200";
     let logoutUrl = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=" + homeUrl;
     document.location.href = logoutUrl;
     callback();
